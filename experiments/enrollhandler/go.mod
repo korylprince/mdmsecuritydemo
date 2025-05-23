774785caf2499d87d2ffa2035d88b05e0f0f3ef1
Kory Prince <korylprince@gmail.com>
2025-05-23 01:09:27 -0500

    add scaffolding for enrollment handler

--- experiments/enrollhandler/go.mod
+++ experiments/enrollhandler/go.mod
@@ -5,15 +5,19 @@ go 1.23.0
 toolchain go1.23.8
 
 require (
+	github.com/gorilla/sessions v1.4.0
 	github.com/korylprince/dep-webview-oidc v0.2.0
 	github.com/samber/slog-http v1.7.0
+	golang.org/x/crypto v0.9.0
 )
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/groob/plist v0.0.0-20220217120414-63fa881b19a5 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	go.mozilla.org/pkcs7 v0.0.0-20210826202110-33d05740a352 // indirect
 	go.opentelemetry.io/otel v1.29.0 // indirect
 	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
--- experiments/enrollhandler/main.go
+++ experiments/enrollhandler/main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"embed"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/fs"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
-	"text/template"
+	texttemplate "text/template"
 
 	_ "embed"
 
+	"github.com/gorilla/sessions"
 	"github.com/korylprince/dep-webview-oidc/header"
+	"github.com/korylprince/mdmsecuritydemo/enrollhandler/machineinfo"
 	sloghttp "github.com/samber/slog-http"
+	"golang.org/x/crypto/argon2"
 )
+
+//go:embed static
+var staticContent embed.FS
+
+//go:embed tmpls/index.html.tmpl
+var startTmplContent string
+
+var startTmpl = template.Must(template.New("").Parse(startTmplContent))
 
 type Enrollment struct {
 	MDMURL        string
@@ -31,37 +45,37 @@ func (e *Enrollment) LogValue() slog.Value {
 		slog.String("apns_topic", e.APNSTopic),
 		slog.String("acme_directory", e.ACMEDirectory),
 		slog.String("acme_key", e.ACMEKey),
-		slog.Group("machine_info",
-			slog.String("origin", string(e.MachineInfo.Origin)),
-			slog.Bool("mdm_can_request_software_update", e.MachineInfo.MDMCanRequestSoftwareUpdate),
-			slog.String("os_version", e.MachineInfo.OSVersion),
-			slog.String("product", e.MachineInfo.Product),
-			slog.String("serial", e.MachineInfo.Serial),
-			slog.String("supplemental_build_version", e.MachineInfo.SupplementalBuildVersion),
-			slog.String("supplemental_os_version_extra", e.MachineInfo.SupplementalOSVersionExtra),
-			slog.String("udid", e.MachineInfo.UDID),
-			slog.String("version", e.MachineInfo.Version),
-		),
 	)
 }
 
 //go:embed enroll.mobileconfig
 var enrollProfile string
 
-var enrollProfileTmpl = template.Must(template.New("").Parse(enrollProfile))
+var enrollProfileTmpl = texttemplate.Must(texttemplate.New("").Parse(enrollProfile))
 
 type Server struct {
+	sessionStore sessions.Store
+
 	allowNoMachineInfo bool
-	dynamicAPIURL      string
-	dynamicAPIKey      string
-	caPool             *x509.CertPool
-	mdmURL             string
-	apnsTopic          string
-	acmeDirectory      string
-	logger             *slog.Logger
+
+	dynamicAPIURL string
+	dynamicAPIKey string
+	caPool        *x509.CertPool
+
+	mdmURL        string
+	apnsTopic     string
+	acmeDirectory string
+
+	logger *slog.Logger
 }
 
 type Option func(*Server)
+
+func WithSessionKeys(keyPairs ...[]byte) Option {
+	return func(s *Server) {
+		s.sessionStore = sessions.NewCookieStore(keyPairs...)
+	}
+}
 
 func WithAllowNoMachineInfo(allow bool) Option {
 	return func(s *Server) {
@@ -105,24 +119,83 @@ func NewServer(opts ...Option) *Server {
 	return s
 }
 
+func (s *Server) Router() (http.Handler, error) {
+	// set up static content handler
+	staticFS, err := fs.Sub(staticContent, "static")
+	if err != nil {
+		return nil, fmt.Errorf("could not create static fs: %w", err)
+	}
+	staticHandler := http.FileServer(http.FS(staticFS))
+
+	mux := http.NewServeMux()
+
+	// entrypoint to enrollment, require machineinfo header
+	mux.Handle("GET /mdm/enroll", machineinfo.SetMachineInfoSession(s.sessionStore, s.allowNoMachineInfo, s.StartHandler()))
+
+	// handle login + webauthn flow
+	mux.Handle("GET /mdm/enroll/login", machineinfo.WithMachineInfoSession(s.sessionStore, s.LoginHandler()))
+
+	// handle forced software update
+	mux.Handle("GET /mdm/enroll/update", machineinfo.WithMachineInfoSession(s.sessionStore, s.SoftwareUpdateHandler()))
+
+	// enrollment profile handler
+	mux.Handle("GET /mdm/enroll/finish", machineinfo.WithMachineInfoSession(s.sessionStore, s.EnrollHandler()))
+
+	// other static files if needed
+	// FIXME: remove this if we don't need other static files
+	mux.Handle("GET /mdm/enroll/", machineinfo.WithMachineInfoSession(s.sessionStore, staticHandler))
+
+	return sloghttp.New(s.logger)(mux), nil
+}
+
+func (s *Server) StartHandler() http.Handler {
+	type tmpl struct {
+		MachineInfo *header.MachineInfo
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: make this nicer, add links to start all other flows, etc
+
+		// get machine info from cookie
+		info := machineinfo.FromContext(r.Context())
+
+		w.Header().Set("Content-Type", "text/html")
+		if err := startTmpl.Execute(w, &tmpl{MachineInfo: info}); err != nil {
+			sloghttp.AddCustomAttributes(r, slog.String("error", fmt.Sprintf("could not render template: %v", err)))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func (s *Server) LoginHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: implement login + webauthn flow
+		// See experiments/webview for webauthn example
+	})
+}
+
+func (s *Server) SoftwareUpdateHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: implement forced update error
+		// documented here: https://github.com/apple/device-management/blob/release/mdm/errors/softwareupdate.required.yaml
+		// and here: https://developer.apple.com/documentation/devicemanagement/errorcodesoftwareupdaterequired
+		// basically return HTTP 403 with the json body
+		// See ergo/mdmenroll for an example of this
+
+		// TODO: if we have time, implement basic GDMF API integration
+	})
+}
+
 func (s *Server) EnrollHandler() http.Handler {
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.TLSClientConfig = &tls.Config{
 		RootCAs: s.caPool,
 	}
 	client := &http.Client{Transport: transport}
-	return sloghttp.New(s.logger)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// parse machine info
-		info, err := header.DefaultParser.Parse(r)
-		if err != nil {
-			if s.allowNoMachineInfo {
-				info = new(header.MachineInfo)
-			} else {
-				sloghttp.AddCustomAttributes(r, slog.String("error", fmt.Sprintf("could not parse machineinfo: %v", err)))
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-		}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// get machine info from cookie
+		info := machineinfo.FromContext(r.Context())
 
 		// get dynamic acme key
 		type response struct {
@@ -165,7 +238,7 @@ func (s *Server) EnrollHandler() http.Handler {
 			sloghttp.AddCustomAttributes(r, slog.String("error", fmt.Sprintf("could not render enrollment profile: %v", err)))
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		}
-	}))
+	})
 }
 
 func run() error {
@@ -178,7 +251,13 @@ func run() error {
 	pool := x509.NewCertPool()
 	pool.AppendCertsFromPEM(buf)
 
+	// use argon2 to stretch passwords to 32 bytes, the needed size for the session library
+	// since we're starting from a secure password, we're not too worried about setting a random salt
+	sessionAuthKey := argon2.IDKey([]byte(os.Getenv("SESSION_AUTH_KEY")), []byte("salt"), 1, 64*1024, 4, 32)
+	sessionEncKey := argon2.IDKey([]byte(os.Getenv("SESSION_ENC_KEY")), []byte("salt"), 1, 64*1024, 4, 32)
+
 	s := NewServer(
+		WithSessionKeys(sessionAuthKey, sessionEncKey),
 		WithAllowNoMachineInfo(os.Getenv("ALLOW_NO_MACHINEINFO") == "true"),
 		WithDynamicAPI(
 			os.Getenv("DYNAMIC_API_URL"),
@@ -193,8 +272,13 @@ func run() error {
 		WithLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil))),
 	)
 
+	router, err := s.Router()
+	if err != nil {
+		return err
+	}
+
 	fmt.Println("listening on :8080")
-	return http.ListenAndServe(":8080", s.EnrollHandler())
+	return http.ListenAndServe(":8080", router)
 }
 
 func main() {
